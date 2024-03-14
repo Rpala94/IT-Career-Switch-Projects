aa01e0c2bd980b2c258f37b78a16792c0eb563c5
Rpala94 <122196216+Rpala94@users.noreply.github.com>
2024-03-14 20:03:43 +0000

    Renamed Projects

---
+++ CodeAcadamy/JavaScirpt Projects/Object Methods/sortArrayMethod2.js
@@ -0,0 +1,17 @@
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+
+// Write your code here:
+
+
+const sortSpeciesByTeeth = arr => arr.sort((speciesObj1, speciesObj2) => speciesObj1.numTeeth > speciesObj2.numTeeth);
+
+
+
+console.log(sortSpeciesByTeeth(speciesArray));
+
+// Should print:
+// [ { speciesName: 'human', numTeeth: 32 },
+//   { speciesName: 'dog', numTeeth: 42 },
+//   { speciesName: 'shark', numTeeth: 50 },
+//   { speciesName: 'alligator', numTeeth: 80 } ]
+
