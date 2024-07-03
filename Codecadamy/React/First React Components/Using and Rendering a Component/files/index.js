3e7cfb9b80614a93685ec288a1c6e6a26505535f
Rpala94 <122196216+Rpala94@users.noreply.github.com>
2024-07-04 00:38:35 +0100

    First React Projects

--- Codecadamy/React/First React Components/Using and Rendering a Component/files/index.js
+++ Codecadamy/React/First React Components/Using and Rendering a Component/files/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import MyComponent from './App';
+import MyComponent from 'App';
 
 
 ReactDOM.createRoot(document.getElementById('app')).render(<MyComponent/>)
---
+++ Codecadamy/React/The State Hook/Use State Setter Outside of JSX/files/App.js
@@ -0,0 +1,7 @@
+import PhoneNumber from "PhoneNumber";
+
+export default function App() {
+  return (
+    <PhoneNumber />
+  );
+}
